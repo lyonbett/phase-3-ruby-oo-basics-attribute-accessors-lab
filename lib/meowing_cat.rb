3d5b67005989729eeddde716484e764ac5974f25
lyonbett <lyon.bett@student.moringaschool.com>
2023-05-18 12:25:33 +0300

    completed assignment

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,15 @@
 ## code your solution here. 
+
+class Cat
+    attr_accessor :name
+    def name=name
+        @name = name
+    end
+    def meow
+        puts "meow!"
+    end
+end
+
+whiskers = Cat.new
+whiskers.name = 'Maru'
+whiskers.meow
